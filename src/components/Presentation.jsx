7c512339b4ecf372ae395b703263a40b8545a7c9
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2023-03-29 17:37:24 +0200

    start presentation component

---
+++ src/components/Presentation.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import cv from "../assets/cv.pdf";
+import satrana from "../assets/satrana.pjg";
+
+const Presentation = () => {
+    
+
+};
+
+export default Presentation;
