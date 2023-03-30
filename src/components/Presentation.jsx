fd6104400efcf6e0433fc047ea1dc6e1a70bf976
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2023-03-30 15:40:37 +0200

    finalization of presentation

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import cv from "../assets/cv.pdf";
-import satrana from "../assets/satrana.pjg";
+import satrana from "./../assets/satrana.jpg";
 
 const Presentation = () => {
-    
-
+    return (
+        <section className="presentation" id="presentationSection">
+            <div className="sectionTitle">
+                <h2>A propos de moi</h2>
+            </div>
+            <div className="presentationContainer">
+        <div className="picSection">
+          <img src={satrana} alt="" className="profilePicture" />
+        </div>
+        <div className="presentation">
+            <div className="presentationP">
+                <p>
+                    Hello! Je m'appelle Satrana, je suis développeur junior, je suis localisée en Ile-de-France. 
+                    <br /> En 2021, j'ai décidé d'exércer un métier
+                    passionnant, porteur et innovant : celui de développeur!
+                    <br /> Après avoir effectué une formation de développeur Front-End chez OpenClassrooms, pendant un an. 
+                    J'ai décidé de poursuivre une formation de développeur C/C++ Linux embarqué pour  <br />
+                    <br /> Je suis actuellement à la recherche d'offres dans la région
+                    Ile-de-France. (Mais je suis aussi à l'écoute pour des offres dans toute la France)
+                     Je suis passionnée par les nouvelles technologies,
+                    le chant, la lecture et la pâtisserie
+                    <br />
+                    <br />
+                    Je suis de nature curieuse, j’aime apprendre et découvrir de nouvelles choses. 
+                    <br /> <br />Bonne visite!
+                </p>
+            </div>
+            <div className="downloadCVButton">
+                <a href={cv} download className="downloadCVLink">
+                Télécharger CV
+                </a>
+            </div>
+        </div>
+      </div>
+        </section>
+    );
 };
 
 export default Presentation;
