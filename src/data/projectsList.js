2765ad3cae147507b0e3d7a0992fe8a998533082
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2023-04-14 17:37:13 +0200

    creating data for projects

---
+++ src/data/projectsList.js
@@ -0,0 +1,66 @@
+export const projectsList = [
+    {
+        name: 'Reservia',
+        id: 1,
+        cover: reservia,
+        description: "Transformer une maquette en site web",
+        techno: [html , css ],
+        link: "https://github.com/SaRvj/SaRvj-SatranaRavelojaona_2_13072021"
+    },
+    {
+        name: 'Oh My Food!',
+        id: 2,
+        cover: ohmyfood,
+        description: "Dynamiser une page web avec des animations CSS",
+        techno: [html , css, sass ],
+        link: "https://github.com/SaRvj/SatranaRavelojaona_3_13072021"
+    },
+    {
+        name: 'GameOn',
+        id: 3,
+        cover: gameon,
+        description: "Offrir une interface facile d'utilisation pour l'inscription aux jeux concours",
+        techno: [html, css, js],
+        link: "https://github.com/SaRvj/SatranaRavelojaona_4_13072021"
+    },
+    {
+        name: 'Les petits plats',
+        id: 4,
+        cover: lespetitsplats,
+        description: "Développer un algorithme de recherche en JavaScript: analyser un problème informatique, développer un algorithme pour résoudre un problème",
+        techno: [html , sass, css, js],
+        link: "https://github.com/SaRvj/SatranaRavelojaona_7_13072021"
+    },
+    {
+        name: 'Billed',
+        id: 5,
+        cover: billed,
+        description: "Corriger les bugs, écrire des tests unitaires avec JavaScript, débugger une application web avec le Chrome Debugger, rédiger un plan de test end-to-end manuel,  Ecrire des tests d'intégration avec JavaScript",
+        techno: [html , css, sass],
+        link:"https://github.com/SaRvj/SatranaRavelojaona_3_13072021"
+    },
+    {
+        name: 'Kasa',
+        id: 6,
+        cover: kasa,
+        description: "Développer une plateforme web pour une entreprise de location",
+        techno: [html , css, js, react ],
+        link:"https://github.com/SaRvj/SatranaRavelojaona_11_13072021"
+    },
+    {
+        name: 'Sportsee',
+        id: 7,
+        cover: sportsee,
+        description: "",
+        techno: [reactjs],
+        link: "https://github.com/SaRvj/SatranaRavelojaona_12_13072021"
+    },
+    {
+        // name: '',
+        // id: 8,
+        // cover: ,
+        // description: "Front-End ",
+        // techno: [html, css, js],
+        // link: ""
+    }
+]
